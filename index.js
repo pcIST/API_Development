5d43f75762572651830fa04ee76ef2d6cbd74673
Anisuzzaman Khan <aniskhan001@yahoo.com>
2018-07-28 19:23:03 +0600

    server routing problem solved

--- index.js
+++ index.js
@@ -1,43 +1,22 @@
-const http = require('http');
+// importing http package
+const http = require('http')
+
+// creating a basic server
 const server = http.createServer( function (req, res) {
-    console.log("server running");
-    // console.log(res);
-    // res.statusCode = 200;
-    res.end("server created");
-    // let name = "IST";
-    // res.json({"name": "pcIST shop", "time": new Date()});
-})
+    console.log("the req URL is: ")
+    console.log(req.url)
 
-server.listen(3000, "localhost", function() {
-    const options = {
-        port: 3000,
-        host: "localhost",
-        path: "/",
-        method: "GET"
-    };
-
-    const req = http.request(options);
-    req.end();
-
-    req.on("end", function (res, socket, upgradeHead) {
-        console.log("user hit the root URL");
-        res.end("user hit the root URL");
-        socket.end();
-    })
-
-})
-
-let shop_option = {
-    port: 3000,
-    host: "localhost",
-    path: "/shop",
-    method: "GET"
-};
-
-http.get(shop_option, (res) => {
-    res.on("end", function (res, socket, upgradeHead) {
-        console.log("user hit the shop URL");
-        res.end("user hit the shop URL");
-        socket.end();
-    })
+    // basic routing
+    if (req.url == "/") {
+        res.end("this is the homepage")
+    } else if (req.url == "/item") {
+        res.statusCode = 200
+        res.end("you are in the item page")
+    } else {
+        res.statusCode = 404
+        res.end("this is NOT the homepage")
+    }
+}).listen(3000, 'localhost', function() {
+    console.log("the port is: ")
+    console.log(server.address().port)
 })
