fbc414059caa0cbf7045fedaff2b12d78e2306f0
arif-btkrm <arif.btkrm@gmail.com>
2018-11-01 01:41:20 +0600

    Data inserting without Validation

--- model/product.js
+++ model/product.js
@@ -16,11 +16,3 @@ const prodSchema = new mongoose.Schema({
 const prodModel = mongoose.model('Product', prodSchema)
 
 module.exports = prodModel; 
-
-
-
-
-//const Cat = mongoose.model('Cat', { name: String });
-
-//const kitty = new Cat({ name: 'Zildjian' });
-//kitty.save().then(() => console.log('meow'));
--- router/product.js
+++ router/product.js
@@ -21,17 +21,17 @@ product_router.get('/lobon', (req, res) => {
 product_router.post('/create', (req, res) => {
 
     const item = new productModel({
-        name: req.body.name,
-        price: req.body.price,
-        brand: req.body.brand,
-        quantity: req.body.quantity,
-        size: req.body.size,
-        unit: req.body.unit,
-        color: req.body.color,
-        created_at: req.body.created_at,
-        expired_date:req.body.expired_date,
-        license: req.body.license,
-        smell: req.body.smell,
+        name : req.body.name,
+        price : req.body.price,
+        brand : req.body.brand,
+        quantity : req.body.quantity,
+        size : req.body.size,
+        unit : req.body.unit,
+        color : req.body.color,
+        created_at : req.body.created_at,
+        expired_date :req.body.expired_date,
+        license : req.body.license,
+        smell : req.body.smell,
     });
     item.save((err)=>{
         if(err){
