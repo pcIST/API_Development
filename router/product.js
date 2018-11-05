e197e5892bdd4efb2f7cc0bc3d62e770db17a1c0
Anisuzzaman Khan <aniskhan001@yahoo.com>
2018-11-05 16:31:43 +0600

    create endoint is now ready

--- router/product.js
+++ router/product.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const prodModel = require('../model/product')
 const product_router = express.Router()
 
 product_router.get('/', (req, res) => {
@@ -17,12 +18,33 @@ product_router.get('/lobon', (req, res) => {
     res.send('ayodin mukto lobon 10 taka')
 })
 
-product_router.post('/create', (req, res) => {
-    console.log("the size is: ")
-    console.log(req.body.size)
+product_router.post('/create', (req, res) => {    
+    let item = new prodModel({
+        "name": req.body.name,
+        "price": req.body.price,
+        "brand": req.body.brand,
+        "quantity": req.body.quantity,
+        "size": req.body.size,
+        "unit": req.body.unit,
+        "color": req.body.color,
+        "expired_date": req.body.expired_date,
+        "license": req.body.license,
+        "smell": req.body.smell
+    });
 
-    console.log("the type of size is:", typeof req.body.size)
-    res.end()
+    // error object definition
+    let client_err = {}
+    item.save( (err) => {
+        if (err) client_err = err
+    })
+
+    if (client_err) {
+        return res.send(422, {
+            "message": "tumi kichu ekta vul korso vai/bon",
+            "error": client_err
+        })
+    }
+    return res.send(201, {"message": "item jog hoyeche, apnake dhonnobad!"})
 })
 
 module.exports = product_router
